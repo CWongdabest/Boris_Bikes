e562e6a5f9bee0f694faec01338ebb1c2a5e2ca9
Tim Jones <timjones10@hotmail.com>
2017-09-27 18:06:51 +0100

    Adding Exercies 8_10

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,8 +1,20 @@
 require 'DockingStation'
 
 describe DockingStation do
+it {is_expected.to respond_to(:release_bike)}
 
-it 'responds to the method release bike' do
-  expect(DockingStation.release_bike).to eq true
-    end
-  end
+# it 'releases a bike' do
+# expect(DockingStation.new.release_bike).to be_instance_of(Bike)
+# end
+
+it 'bike is working' do
+  bike = DockingStation.new.release_bike
+  expect(bike).to respond_to(:working?)
+end
+
+end
+
+# it 'responds to the method release bike' do
+#   expect(DockingStation.new).to respond_to(:release_bike)
+# #     end
+#   end
